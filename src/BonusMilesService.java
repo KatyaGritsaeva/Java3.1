86ebf20790de884515fe31ba7c76517863aadbdf
KatyaGritsaeva <gekaterina2019@gmail.com>
2022-03-27 22:42:05 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+
+     //Входные данные
+        int amount = 20;
+
+        // Логика программы
+        int bonus = price / amount;
+        return bonus;
+    }
+
+}
